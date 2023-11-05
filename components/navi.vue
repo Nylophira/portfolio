8ce864cfa89c59dc9f87aa0ba4767581b9704e72
Nylophira <121953951+Nylophira@users.noreply.github.com>
2023-11-05 16:48:38 +0100

    css nav

--- components/navi.vue
+++ components/navi.vue
@@ -6,13 +6,16 @@
 
 //Changement de format en fonction de la localisation
 const entree = ref("entree");
+const actif = ref("entree");
 const route = useRoute();
 
     watch(() => route.path, (newPath) => {
       if (newPath === '/') {
         entree.value = 'entree';
+        actif.value = 'entree';
       } else {
         entree.value = 'autre';
+        actif.value = newPath;
       }
     });
 
@@ -20,14 +23,30 @@ const route = useRoute();
 </script>
 
 <template>
-    <nav class="nav nav-underline" :class=entree>
-        <ul class="nav nav-underline">       
-            <li class="nav-item" v-if="entree=='autre'"><NuxtLink to="/" class="nav-link" >Accueil</NuxtLink></li>
-            <li class="nav-item"><NuxtLink to="/pres" class="nav-link" >Présentation</NuxtLink></li>
-            <li class="nav-item"><NuxtLink to="/projets" class="nav-link">Projets</NuxtLink></li>
-            <li class="nav-item"><NuxtLink to="/competences" class="nav-link">Compétences</NuxtLink></li>
-            <li class="nav-item"><NuxtLink to="/contact" class="nav-link">CV</NuxtLink></li>
-            <li class="nav-item"><NuxtLink to="/contact" class="nav-link">Contact</NuxtLink></li>
+    <!--  :class="actif=='/pres'? 'active':''"  
+        :class="actif=='/projets'? 'active':''"
+         :class="actif=='/competences'? 'active':''"
+        ça fonctionne mais y a un problème sur le cv / contact du coup voir pour prendre les classes de vue plutôt ?-->
+    <nav :class=entree class="px-3">
+        <ul class="nav nav-underline justify-content-end">       
+            <li class="nav-item" v-if="entree=='autre'">
+                <NuxtLink to="/" class="nav-link">Accueil</NuxtLink>
+            </li>
+            <li class="nav-item ms-auto">
+                <NuxtLink to="/pres" class="nav-link" >Présentation</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink to="/projets" class="nav-link" >Projets</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink to="/competences" class="nav-link">Compétences</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink to="/contact" class="nav-link">CV</NuxtLink>
+            </li>
+            <li class="nav-item">
+                <NuxtLink to="/contact" class="nav-link">Contact</NuxtLink>
+            </li>
         </ul>
     </nav>
 </template>
@@ -35,17 +54,31 @@ const route = useRoute();
 <style lang="scss">
 
 .entree {
-    background-color: $rFonce;
+    background-color: $rouge;
 
-   .nav .nav-link {
-        color: white;
+   .nav {
+        
+        .nav-link {
+            color: white;
+            text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); 
+        }
+        .nav-link:hover {
+            color: white;
+        }
     }
 }
 
 .nav {
     .nav-link {
         color: rgb(0, 0, 0);
+        font-family: 'DM Serif Display';
+       
+    }
+
+    .nav-link:hover {
+        color: $rouge;
     }
 }
+
 
 </style>
