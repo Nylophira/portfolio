da55ea5fdecca0044cda92c0fa99a047b737a44d
Nylophira <121953951+Nylophira@users.noreply.github.com>
2023-12-15 13:14:20 +0100

    ajout porte icone accueil

--- components/navi.vue
+++ components/navi.vue
@@ -4,6 +4,7 @@ const route = useRoute();
 
 //Changement de format en fonction de la localisation
 const entree = ref("entree");
+const couleur = ref(true);
 
 ///en cas d'actualisation de la page
 if (route.path == "/") {
@@ -11,7 +12,7 @@ if (route.path == "/") {
 } else {
     entree.value = 'autre';
 }
-//const actif = ref("");
+
 
 //en cas de changement de page
 watch(() => route.path, (newPath) => {
@@ -21,12 +22,6 @@ watch(() => route.path, (newPath) => {
         entree.value = 'autre';
     }
 });
-
-//soulignement du cv ou du contact
-/* function cvCont(qui) {
-    actif.value = qui;
-}  */
-
 
 
 //ouverture/fermeture des mentions légales
@@ -45,14 +40,12 @@ const ouverture = () => {
     
     ouvre.value = true;
     emit('change', ouvre.value)
-   //emit('change', 'ferme');
 }
 
 //responsive tablette / ordi
 const show = ref(props.menu);
 const navLigne = ref("justify-content-end align-items-center w-100");
 const navCol = ref("position-absolute");
-//const menuTabl = ref("");
 
 const menuO = () => {
     if (show.value) {
@@ -74,14 +67,16 @@ watch(() => props.menu, (newValue) => {
       show.value = newValue;
     });
 
+watch(() => couleur, (newValue) => {
+    couleur.value = newValue;
+});
+
 
 </script>
 
 <template>
     <nav v-if="!((entree=='entree')&&(show=='show'))" :class="(entree=='entree' ? 'entree d-none d-lg-flex':'')" class="px-3 pt-1 navbar navbar-expand-lg">
         <div class="container-fluid">
-            <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-            <!-- il va falloir trouver un truc pour que quand on clique ça enlève show -->
             <button @click=menuO class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -89,8 +84,9 @@ watch(() => props.menu, (newValue) => {
                 <ul class="navbar-nav nav-underline d-flex" :class="show ? navCol : navLigne">
                     <li @click="clickMobile" class="nav-item" v-if="entree=='autre'" :class="show ? 'px-4' : ''">
                         <NuxtLink to="/" class="nav-link icone icon-link ">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  fill="currentColor" class="bi bi-house" viewBox="0 0 16 16" @mouseover="() => couleur = false" @mouseleave="() => couleur = true" @click="() => couleur = true">
                                 <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+                                <rect x="6" y="9" width="4" height="6" :fill="couleur ? 'currentColor':'none'" :stroke="couleur ? '':'currentColor'"/>
                             </svg>
                         </NuxtLink>
                     </li>
@@ -103,9 +99,6 @@ watch(() => props.menu, (newValue) => {
                     <li @click="clickMobile" class="nav-item px-4">
                         <NuxtLink to="/competences" class="nav-link">Compétences</NuxtLink>
                     </li>
-                <!--  <li class="nav-item px-4">
-                        <NuxtLink to="/contact" class="nav-link" :class="actif==='CV'? '':'non'" @click="cvCont('CV')">CV</NuxtLink>
-                    </li> -->
                     <li @click="clickMobile" class="nav-item px-4">
                         <NuxtLink to="/contact" class="nav-link">Contact</NuxtLink>
                     </li>
@@ -120,16 +113,7 @@ watch(() => props.menu, (newValue) => {
 
 <style lang="scss">
 
-//Navigation entre les pages
-/* .page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
-} */
+
 
 .entree {
     background-color: $rouge;
@@ -192,18 +176,5 @@ watch(() => props.menu, (newValue) => {
     }
     
 }
-
-/* .entree {
-
-    .collapse.navbar-collapse.show {
-
-    ul {
-        background-color: $rouge;
-    }
-    
-}
-} */
-
-
 
 </style>
